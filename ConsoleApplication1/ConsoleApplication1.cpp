0abfb72cbb035c7fd35fe27e44a6c72efc5525b9
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-02-27 15:28:27 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    float alas, tinggi, luas;
+    cout << "masukan alas =";
+    cin >> alas;
+    cout << "masukan tinggi =";
+    cin >> tinggi;
+    luas = alas * tinggi / 2;   
+    cout << "luasnya =" << luas;
+}
+
